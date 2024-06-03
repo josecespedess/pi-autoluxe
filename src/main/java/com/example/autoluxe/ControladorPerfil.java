aa126f586bb7d03d8c78c350fdb70a4ba43e6588
salvalh-acciosoft <salva.lopez.dam@gmail.com>
2024-06-03 14:51:48 +0200

    Codigo arreglado

--- src/main/java/com/example/autoluxe/ControladorPerfil.java
+++ src/main/java/com/example/autoluxe/ControladorPerfil.java
@@ -194,12 +194,11 @@ public class ControladorPerfil implements Initializable {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("vista_InicioSesion.fxml"));
             Parent root = loader.load();
-//          Stage nuevaVentana = new Stage();
-//          nuevaVentana.setTitle("AutoLuxe"); // Puedes establecer el título
-//          nuevaVentana.setScene(new Scene(root,1920,1000));
-//          Stage ventanaActual = (Stage) btnCerrarSesion.getScene().getWindow();
-//          ventanaActual.close();
-//          nuevaVentana.show();
+            Stage nuevaVentana = new Stage();
+            nuevaVentana.setTitle("AutoLuxe");
+            nuevaVentana.setScene(new Scene(root,1920,1000));
+            Stage ventanaActual = (Stage) btnCerrarSesion.getScene().getWindow();
+            ventanaActual.close();nuevaVentana.show();
             contenedor.getChildren().setAll(root);
         } catch (Exception e) {
             e.printStackTrace();
