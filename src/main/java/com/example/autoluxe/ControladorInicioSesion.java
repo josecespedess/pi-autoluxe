8ec5cfe03079f471150f94c7f9655f88592e2df1
salvalh-acciosoft <salva.lopez.dam@gmail.com>
2024-06-03 14:47:26 +0200

    Codigo arreglado

--- src/main/java/com/example/autoluxe/ControladorInicioSesion.java
+++ src/main/java/com/example/autoluxe/ControladorInicioSesion.java
@@ -48,61 +48,47 @@ public class ControladorInicioSesion implements Initializable {
         bd.conectar();
         msgError1.setVisible(false);
         msgError2.setVisible(false);
-        if(edEmail.getText().isEmpty()||edContraseña.getText().isEmpty()||edEmail.getText().contains("@autoluxe.com")==false)
-        {
-            if (edEmail.getText().isEmpty()||edEmail.getText().contains("@")==false)
-            {
+        if (edEmail.getText().isEmpty() || edContraseña.getText().isEmpty() || edEmail.getText().contains("@autoluxe.com") == false) {
+            if (edEmail.getText().isEmpty() || edEmail.getText().contains("@") == false) {
                 msgError1.setVisible(true);
             }
-            if(edContraseña.getText().isEmpty())
-            {
+            if (edContraseña.getText().isEmpty()) {
                 msgError2.setVisible(true);
             }
-        }
-        else
-        {
-            String correo="";
-            String contrasena="";
-            try(Connection conexion=bd.getConnection())
-            {
-                String query="SELECT correo,contraseña FROM empleados WHERE correo='"+edEmail.getText()+"'";
-                Statement st=conexion.createStatement();
-                ResultSet rs=st.executeQuery(query);
-                while (rs.next())
-                {
-                    correo=rs.getString("correo");
-                    contrasena=rs.getString("contraseña");
+        } else {
+            String correo = "";
+            String contrasena = "";
+            try (Connection conexion = bd.getConnection()) {
+                String query = "SELECT correo,contraseña FROM empleados WHERE correo='" + edEmail.getText() + "'";
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                while (rs.next()) {
+                    correo = rs.getString("correo");
+                    contrasena = rs.getString("contraseña");
                 }
-                if(correo!="")
-                {
-                    if(contrasena.equals(edContraseña.getText()))
-                    {
+                if (correo != "") {
+                    if (contrasena.equals(edContraseña.getText())) {
                         abrirAplicacion();
-                    }
-                    else
-                    {
+                    } else {
                         Alert alert = new Alert(Alert.AlertType.ERROR);
                         alert.setTitle("Diálogo de Alerta");
                         alert.setHeaderText("Información Incorrecta");
                         alert.setContentText("Contraseña incorrecta, estos datos no son válidos.");
                         alert.showAndWait();
                     }
-                }
-                else
-                {
+                } else {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Diálogo de Alerta");
                     alert.setHeaderText("Información Incorrecta");
                     alert.setContentText("Correo incorrecto, estos datos no son existentes.");
                     alert.showAndWait();
                 }
-            }
-            catch(SQLException e)
-            {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
     }
+
     @FXML
     private void abrirAplicacion() {
         try {
@@ -110,7 +96,7 @@ public class ControladorInicioSesion implements Initializable {
             Parent root = loader.load();
             Stage nuevaVentana = new Stage();
             nuevaVentana.setTitle("AutoLuxe"); // Puedes establecer el título
-            nuevaVentana.setScene(new Scene(root,1920,1080));
+            nuevaVentana.setScene(new Scene(root, 1920, 1080));
             nuevaVentana.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/LogoAutoLuxe.png")));
             Stage ventanaActual = (Stage) bt1.getScene().getWindow();
             ventanaActual.close();
@@ -119,9 +105,9 @@ public class ControladorInicioSesion implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
-    public void abrirGmail()
-    {
+    public void abrirGmail() {
         String url = "https://www.google.com/intl/es/gmail/about/";
         try {
             java.awt.Desktop.getDesktop().browse(new URI(url));
@@ -129,9 +115,9 @@ public class ControladorInicioSesion implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
-    public void abrirInstagram()
-    {
+    public void abrirInstagram() {
         String url = "https://www.instagram.com/auto_luxe_js/";
         try {
             java.awt.Desktop.getDesktop().browse(new URI(url));
@@ -139,9 +125,9 @@ public class ControladorInicioSesion implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
-    public void abrirFacebook()
-    {
+    public void abrirFacebook() {
         String url = "https://www.facebook.com/profile.php?id=61558487721041";
         try {
             java.awt.Desktop.getDesktop().browse(new URI(url));
@@ -149,6 +135,4 @@ public class ControladorInicioSesion implements Initializable {
             e.printStackTrace();
         }
     }
-
-
 }
